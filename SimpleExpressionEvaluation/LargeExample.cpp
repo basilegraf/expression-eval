8f512dbfaf2d593ff3bfca567284c1ba835aef4b
Basile <basile.graf@a3.epfl.ch>
2021-04-13 13:24:57 +0200

    Merged ExprTk comparison. Using a preprocessor define to switch on or off the ExprTk compilation and comparison. This also allows to compare compile time and executable size.

--- SimpleExpressionEvaluation/LargeExample.cpp
+++ SimpleExpressionEvaluation/LargeExample.cpp
@@ -81,6 +81,79 @@ std::string getLargeExampleExpression()
         "fDFC[5] = PDFC[1]*(PDFC[9]*tmp133 + PDFC[9]*tmp134 + tmp109*tmp141 + tmp112*tmp141 - tmp121*tmp142 + tmp122*tmp142) + PDFC[8]*tmp129 - thetatt*(-PDFC[2] - PDFC[3] + PDFC[8]*tmp119 + tmp138 + tmp139) + xitt*(PDFC[3] + pow(PDFC[8], 2)*tmp140 + PDFC[9]*tmp114*tmp140 - 2.0*tmp138 - tmp139);";
     return expression_string;
 }
+
+
+
+// Expression version for the ExprTk lib
+std::string getLargeExampleExpressionExprtk()
+{
+    std::string expression_string =
+        "var H := sqrt(pow(X[1], 2) + pow(PDFC[8] + X[0], 2));"
+        "var Ht := (PDFC[8]*Xt[0] + X[0]*Xt[0] + X[1]*Xt[1])/H;"
+        "var Htt := (-pow(Ht, 2) + PDFC[8]*Xtt[0] + X[0]*Xtt[0] + X[1]*Xtt[1] + pow(Xt[0], 2) + pow(Xt[1], 2))/H;"
+        "var omega := acos(0.5*(pow(H, 2) - pow(PDFC[10], 2) + pow(PDFC[9], 2))/(H*PDFC[9]));"
+        "var omegat := Ht*(-H + PDFC[9]*cos(omega))/(H*PDFC[9]*sin(omega));"
+        "var tmp100 := sin(omega);"
+        "var tmp101 := PDFC[9]*cos(omega);"
+        "var omegatt := (-H*Htt - H*pow(omegat, 2)*tmp101 - pow(Ht, 2) - 2*Ht*PDFC[9]*omegat*tmp100 + Htt*tmp101)/(H*PDFC[9]*tmp100);"
+        "var xi := acos(0.5*(-pow(H, 2) + pow(PDFC[10], 2) + pow(PDFC[9], 2))/(PDFC[10]*PDFC[9]));"
+        "var xit := H*Ht/(PDFC[10]*PDFC[9]*sin(xi));"
+        "var xitt := (H*Htt + pow(Ht, 2) - PDFC[10]*PDFC[9]*pow(xit, 2)*cos(xi))/(PDFC[10]*PDFC[9]*sin(xi));"
+        "var alpha := atan2(X[1], PDFC[8] + X[0]);"
+        "var tmp102 := cos(alpha);"
+        "var tmp103 := sin(alpha);"
+        "var alphat := (-Xt[0]*tmp103 + Xt[1]*tmp102)/(PDFC[8]*tmp102 + X[0]*tmp102 + X[1]*tmp103);"
+        "var tmp104 := cos(alpha);"
+        "var tmp105 := sin(alpha);"
+        "var tmp106 := 2*alphat;"
+        "var tmp107 := pow(alphat, 2);"
+        "var tmp108 := tmp105*tmp107;"
+        "var alphatt := (PDFC[8]*tmp108 + X[0]*tmp108 - X[1]*tmp104*tmp107 - Xt[0]*tmp104*tmp106 - Xt[1]*tmp105*tmp106 - Xtt[0]*tmp105 + Xtt[1]*tmp104)/(PDFC[8]*tmp104 + X[0]*tmp104 + X[1]*tmp105);"
+        "var theta := alpha + omega;"
+        "var thetat := alphat + omegat;"
+        "var thetatt := alphatt + omegatt;"
+        "var tmp109 := pow(thetat, 2);"
+        "var tmp110 := theta + xi;"
+        "var tmp111 := PDFC[6]*cos(tmp110);"
+        "var tmp112 := 2*thetat*xit;"
+        "var tmp113 := pow(xit, 2);"
+        "var tmp114 := PDFC[8]*cos(xi);"
+        "var tmp115 := tmp111 - tmp114;"
+        "var tmp116 := PDFC[1]*(tmp109*tmp111 + tmp111*tmp112 + tmp113*tmp115);"
+        "var tmp117 := cos(theta);"
+        "var tmp118 := PDFC[5]*tmp117;"
+        "var tmp119 := PDFC[0]*tmp118;"
+        "var tmp120 := tmp109*tmp119;"
+        "var tmp121 := PDFC[8]*sin(xi);"
+        "var tmp122 := PDFC[6]*sin(tmp110);"
+        "var tmp123 := -tmp121 + tmp122;"
+        "var tmp124 := PDFC[1]*xitt;"
+        "var tmp125 := sin(theta);"
+        "var tmp126 := PDFC[5]*tmp125;"
+        "var tmp127 := PDFC[0]*tmp126;"
+        "var tmp128 := thetatt*(PDFC[1]*tmp122 - tmp127) + tmp123*tmp124;"
+        "var tmp129 := tmp109*tmp127;"
+        "var tmp130 := tmp115*tmp124;"
+        "var tmp131 := PDFC[1]*tmp111;"
+        "var tmp132 := thetatt*(-tmp119 + tmp131);"
+        "var tmp133 := tmp109*tmp122;"
+        "var tmp134 := tmp112*tmp122;"
+        "var tmp135 := PDFC[1]*PDFC[7];"
+        "var tmp136 := PDFC[0]*PDFC[7];"
+        "var tmp137 := PDFC[6]*PDFC[8];"
+        "var tmp138 := PDFC[1]*tmp117*tmp137;"
+        "var tmp139 := PDFC[9]*tmp131;"
+        "var tmp140 := +PDFC[1];"
+        "var tmp141 := tmp125*tmp137;"
+        "var tmp142 := PDFC[9]*tmp113;"
+        "fDFC[0] := tmp116 - tmp120 + tmp128;"
+        "fDFC[1] := PDFC[1]*(tmp113*tmp123 + tmp133 + tmp134) - tmp129 - tmp130 - tmp132;"
+        "fDFC[2] := -tmp135 - tmp136;"
+        "fDFC[3] := PDFC[1]*PDFC[4]*(tmp113*(tmp121 - tmp122) - tmp133 - tmp134) + PDFC[4]*tmp129 + PDFC[4]*(tmp130 + tmp132) + tmp123*tmp135 - tmp126*tmp136;"
+        "fDFC[4] := PDFC[4]*tmp116 - PDFC[4]*tmp120 + PDFC[4]*tmp128 + tmp135*(PDFC[9] - tmp111 + tmp114) + tmp136*(-PDFC[8] + tmp118);"
+        "fDFC[5] := PDFC[1]*(PDFC[9]*tmp133 + PDFC[9]*tmp134 + tmp109*tmp141 + tmp112*tmp141 - tmp121*tmp142 + tmp122*tmp142) + PDFC[8]*tmp129 - thetatt*(-PDFC[2] - PDFC[3] + PDFC[8]*tmp119 + tmp138 + tmp139) + xitt*(PDFC[3] + pow(PDFC[8], 2)*tmp140 + PDFC[9]*tmp114*tmp140 - 2.0*tmp138 - tmp139);";
+    return expression_string;
+}
  
 
  
--- SimpleExpressionEvaluation/main.cpp
+++ SimpleExpressionEvaluation/main.cpp
@@ -11,14 +11,19 @@
 #include "ExpressionParser.h"
 #include "ExpressionCompiler.h"
 
-
 #include "LargeExample.h"
+
+// #define COMPILE_WITH_EXPRTK // Large executable size and compile time
+
+#ifdef COMPILE_WITH_EXPRTK
+#include "exprtk.hpp" // Available at: https://github.com/ArashPartow/exprtk
+#endif
 
 void func(double* z, double* x, double* y)
 {
     using namespace std;
     double aaa = -3.5543*cos(-(*x) + 1 + 2.*(-sin(+y[0]) + .1+y[1]))+ 2;
-    *z=-cos(aaa + (*x)*y[0]) + pow(*x, 2) + pow(y[0], *x) + pow(y[1], 2.4);
+    *z=-cos(aaa + (*x)*y[0]) + pow(y[0], y[1]) + pow(y[0], 2);
 }
 
 
@@ -46,7 +51,7 @@ int main(int argc, char **argv)
         
         std::string sexpr = 
             "aaa=-3.5543*cos(-x + 1 + 2.*(-sin(+y[0]) + .1+y[1]))+ 2;"
-            "z=-cos(aaa + x*y[0]) + pow(x, 2) + pow(y[0], x) + pow(y[1], 2.4)";
+            "z=-cos(aaa + x*y[0]) + pow(y[0], y[1]) + pow(y[0], 2)";
         
         /*std::cout << "Expression: " << sexpr << "\n";
         
@@ -57,6 +62,8 @@ int main(int argc, char **argv)
         tree.Print();
         std::cout << "\n";*/
         
+        std::cout << "Small expression:\n"; 
+                
         // Compile expression
         expreval::Compiler compiler;
         
@@ -70,6 +77,7 @@ int main(int argc, char **argv)
         
 
         compiler.CompileExpression(sexpr);
+        std::cout << "Number of operations : " << compiler.GetNumOperations() << "\n";
         
         x = 0.4;
         auto t0 = tic();
@@ -99,6 +107,7 @@ int main(int argc, char **argv)
         
         std::cout << "\n";
         
+        std::cout << "Large expression:\n"; 
         //////// Large example       
         // Symbols
         expreval::expr_val_t X[] = {0.03, 0.6};
@@ -106,8 +115,11 @@ int main(int argc, char **argv)
         expreval::expr_val_t Xtt[] = {0.33, 0.45};
         // PDFC = [mW, mD, iWz, iDz, cogz, Wcogx, Dcogx, g, E, W, D]
         expreval::expr_val_t PDFC[] = {0.2, 0.4, 0.1, 0.12, 0.2, 0.25, 0.27, 9.8, 0.1, 0.5, 0.5};
-        expreval::expr_val_t fDFC[6] = {0.0};
-        expreval::expr_val_t fDFC2[6] = {0.0};
+        expreval::expr_val_t fDFCEval[6] = {0.0};
+        expreval::expr_val_t fDFCNative[6] = {0.0};
+        #ifdef COMPILE_WITH_EXPRTK
+        expreval::expr_val_t fDFCTK[6] = {0.0};
+        #endif
         
         std::string sexprL = getLargeExampleExpression();
         
@@ -118,7 +130,7 @@ int main(int argc, char **argv)
         compilerL.RegisterSymbol("Xt", Xt, 2);
         compilerL.RegisterSymbol("Xtt", Xtt, 2);
         compilerL.RegisterSymbol("PDFC", PDFC, 11);
-        compilerL.RegisterSymbol("fDFC", fDFC, 6);
+        compilerL.RegisterSymbol("fDFC", fDFCEval, 6);
         
         t0 = tic();
         compilerL.CompileExpression(sexprL);
@@ -126,6 +138,29 @@ int main(int argc, char **argv)
         std::cout << "Compile time = " << compileTime << "s\n";
         std::cout << "Number of operations : " << compilerL.GetNumOperations() << "\n";
         
+        
+        #ifdef COMPILE_WITH_EXPRTK
+        // Compile expression with ExprTK
+        auto sexprLTK_string = getLargeExampleExpressionExprtk();
+        
+        exprtk::symbol_table<double> symbol_table;
+        symbol_table.add_vector("X", X, 2);
+        symbol_table.add_vector("Xt", Xt, 2);
+        symbol_table.add_vector("Xtt", Xtt, 2);
+        symbol_table.add_vector("PDFC", PDFC, 11);
+        symbol_table.add_vector("fDFC", fDFCTK, 6);
+
+        exprtk::expression<double> expressionTK;
+        expressionTK.register_symbol_table(symbol_table);
+
+        t0 = tic();
+        exprtk::parser<double> parser;
+        parser.compile(sexprLTK_string, expressionTK);
+        double compileTimeTK = toc(t0);
+        std::cout << "Compile time ExprTK = " << compileTimeTK << "s\n";    
+        #endif
+        
+        // Evaluate
         nRuns = 1000;
         X[0] = 0.03;
         t0 = tic();
@@ -136,28 +171,56 @@ int main(int argc, char **argv)
         }
         double durEvalL = toc(t0);
         
+        
+        #ifdef COMPILE_WITH_EXPRTK
+        // Evaluate with ExprTK
         X[0] = 0.03;
         t0 = tic();
         for (int k = 0; k<nRuns; k++)
         {
             X[0] += 1.0e-9;
-            LargeExampleFunc(X, Xt, Xtt, PDFC, fDFC2);
+            expressionTK.value();
+        }
+        double durEvalLTK = toc(t0);
+        #endif
+        
+        // Evaluate Native
+        X[0] = 0.03;
+        t0 = tic();
+        for (int k = 0; k<nRuns; k++)
+        {
+            X[0] += 1.0e-9;
+            LargeExampleFunc(X, Xt, Xtt, PDFC, fDFCNative);
         }
         double durNativeL = toc(t0);
         
-        std::cout << "Time eval large example   = " << 1.0e6 * durEvalL / nRuns << "[us]" << std::endl;
+        std::cout << "Time eval   large example = " << 1.0e6 * durEvalL / nRuns << "[us]" << std::endl;
+        #ifdef COMPILE_WITH_EXPRTK
+        std::cout << "Time ExprTK large example = " << 1.0e6 * durEvalLTK / nRuns << "[us]" << std::endl;
+        #endif
         std::cout << "Time native large example = " << 1.0e6 * durNativeL / nRuns << "[us]" << std::endl;
-        std::cout << "Ratio = " << double(durEvalL) / double(durNativeL) << std::endl;
+        std::cout << "Ratio eval   = " << double(durEvalL) / double(durNativeL) << std::endl;
+        #ifdef COMPILE_WITH_EXPRTK
+        std::cout << "Ratio ExprTK = " << double(durEvalLTK) / double(durNativeL) << std::endl;
+        #endif
         
         for (int k = 0; k<6; k++)
         {
-            std::cout << fDFC[k] << ", ";
+            std::cout << fDFCEval[k] << ", ";
         }
         std::cout << "\n";
         
+        #ifdef COMPILE_WITH_EXPRTK
         for (int k = 0; k<6; k++)
         {
-            std::cout << fDFC2[k] << ", ";
+            std::cout << fDFCTK[k] << ", ";
+        }
+        std::cout << "\n";
+        #endif
+        
+        for (int k = 0; k<6; k++)
+        {
+            std::cout << fDFCNative[k] << ", ";
         }
         std::cout << "\n";
 
